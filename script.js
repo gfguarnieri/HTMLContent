183bbdc690fabf8dcc0887b4c35964abcf79718d
Giovanni Guarnieri <gfguarnieri@hotmail.com>
2024-06-14 01:27:57 -0300

    :tada: first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+hljs.initHighlightingOnLoad();
+hljs.initLineNumbersOnLoad();
+
+const allRegion = document.querySelectorAll('.region');
+allRegion.forEach((region, i) => {
+    if (i === allRegion.length - 1) return;
+    region.setAttribute('data-aos', i % 2 === 0 ? 'fade-left' : 'fade-right');
+});
+
+const allTitles = document.querySelectorAll('.title');
+allTitles.forEach((title, i) => {
+    const option = document.createElement('li');
+    option.innerHTML = title.textContent;
+    option.addEventListener('click', () => {
+        title.scrollIntoView({ behavior: 'smooth' });
+    });
+    document.querySelector('.options').appendChild(option);
+});
+
+const btnToggle = document.querySelector('.toggle');
+const nav = document.querySelector('nav');
+btnToggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
+
+AOS.init();
